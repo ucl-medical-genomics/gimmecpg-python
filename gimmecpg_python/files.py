e4267cb4d05262d41a1a58b1d4ea8274801b0bd8
niuums <niuzhengchai@gmail.com>
2024-05-09 16:47:25 +0100

    Break up read_files()

--- gimmecpg_python/files.py
+++ gimmecpg_python/files.py
@@ -5,6 +5,48 @@ from pathlib import Path
 import polars as pl
 
 
+def collapse_strands(bed):
+    """Collapse strands."""
+    pos = bed.filter(pl.col("strand") == "+").with_columns(
+            (pl.col("end")).alias("reverse_start")
+    )  # add column for start site on complementary strand
+    neg = bed.filter(pl.col("strand") == "-")
+
+        # wCompStart = bed.with_columns((pl.col('end') + 1)
+        # .alias('reverse_start')) # if data is 1-index Start side
+
+    joint = neg.join(
+        pos, left_on=["chr", "start"], right_on=["chr", "reverse_start"], how="outer_coalesce"
+    ).with_columns(pl.concat_str([pl.col("strand"), pl.col("strand_right")], separator="/", ignore_nulls=True))
+
+    merged = (
+        joint.with_columns(
+            pl.min_horizontal("start", "start_right").alias("start"),
+            pl.max_horizontal("end", "end_right").alias("end"),
+            pl.col(["percent_methylated_right", "coverage_right", "percent_methylated", "coverage"])
+            .fill_null(0)
+            .cast(pl.UInt16),
+        )
+        .with_columns(
+            (
+                (
+                    pl.col("coverage") * pl.col("percent_methylated")
+                    + pl.col("coverage_right") * pl.col("percent_methylated_right")
+                )
+                / (pl.col("coverage") + pl.col("coverage_right"))
+            ).alias("avg")
+        )
+        .with_columns(
+            pl.when(pl.col("strand_right").is_not_null())
+            .then(((pl.col("coverage") + pl.col("coverage_right")) / 2).alias("avg_coverage"))
+            .otherwise(pl.col("coverage").alias("avg_coverage"))
+        )  # calculated weighted average
+
+    )
+    return merged
+
+
+
 def read_files(file, mincov, collapse):
     """Scan files."""
     name = Path(file).stem
@@ -37,54 +79,24 @@ def read_files(file, mincov, collapse):
                 "column_11": "percent_methylated",
             }
         )  # rename to something that makes more sense
+        .with_columns(
+            pl.col("chr").str.replace(r"(?i)Chr", "") # remove "chr" from Chr column to match reference
+)
     )
 
     if collapse:
-        pos = bed.filter(pl.col("strand") == "+").with_columns(
-            (pl.col("end")).alias("reverse_start")
-        )  # add column for start site on complementary strand
-        neg = bed.filter(pl.col("strand") == "-")
-
-        # wCompStart = bed.with_columns((pl.col('end') + 1)
-        # .alias('reverse_start')) # if data is 1-index Start side
-
-        joint = neg.join(
-            pos, left_on=["chr", "start"], right_on=["chr", "reverse_start"], how="outer_coalesce"
-        ).with_columns(pl.concat_str([pl.col("strand"), pl.col("strand_right")], separator="/", ignore_nulls=True))
-
-        results = (
-            joint.with_columns(
-                pl.min_horizontal("start", "start_right").alias("start"),
-                pl.max_horizontal("end", "end_right").alias("end"),
-                pl.col(["percent_methylated_right", "coverage_right", "percent_methylated", "coverage"])
-                .fill_null(0)
-                .cast(pl.UInt16),
-            )
-            .with_columns(
-                (
-                    (
-                        pl.col("coverage") * pl.col("percent_methylated")
-                        + pl.col("coverage_right") * pl.col("percent_methylated_right")
-                    )
-                    / (pl.col("coverage") + pl.col("coverage_right"))
-                ).alias("avg")
-            )
-            .with_columns(
-                pl.when(pl.col("strand_right").is_not_null())
-                .then(((pl.col("coverage") + pl.col("coverage_right")) / 2).alias("avg_coverage"))
-                .otherwise(pl.col("coverage").alias("avg_coverage"))
-            )  # calculated weighted average
-        )
+        data = collapse_strands(bed)
     else:
-        results = bed.with_columns(pl.col("percent_methylated").alias("avg"), pl.col("coverage").alias("avg_coverage"))
+        data = bed.with_columns(pl.col("percent_methylated").alias("avg"), pl.col("coverage").alias("avg_coverage"))
 
-    cov_results = (
-        results.filter(pl.col("avg_coverage") > mincov)
+    data_cov_filt = (
+        data.filter(pl.col("avg_coverage") > mincov)
         .select(["chr", "start", "end", "strand", "avg"])
         .with_columns(pl.lit(name).alias("sample"))
     )  # filter by coverage
 
-    return cov_results
+    return data_cov_filt
+
 
 
 def save_files_normal(file, outpath):
@@ -96,6 +108,7 @@ def save_files_normal(file, outpath):
     )
     outfile = Path(outpath, "imputed_" + filename + ".bed")
     file.write_csv(outfile, separator="\t")
+    print(f"{filename} saved")
 
 
 def save_files_streaming(file, outpath):
@@ -108,3 +121,4 @@ def save_files_streaming(file, outpath):
     )
     outfile = Path(outpath, "imputed_" + filename + ".bed")
     file.write_csv(outfile, separator="\t")
+    print(f"{filename} saved")
